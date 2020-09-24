699723b4ebe706ca61127f2fde89633b3878ee1e
Ishan0071 <70375286+Ishan0071@users.noreply.github.com>
2020-09-24 14:44:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="https://i.ibb.co/k80kB58/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <meta name="theme-color" content="paleturquoise">
+    <title>Cyber Kavach</title>
+    <style>
+        an.hover {
+            text-decoration: underliner;
+            color: darkgoldenrod;
+        }
+         ::-webkit-scrollbar {
+            width: 10px;
+        }
+         ::-webkit-scrollbar-track {
+            background: white;
+        }
+         ::-webkit-scrollbar-thumb {
+            background: gainsboro;
+        }
+         ::-webkit-scrollbar-thumb:hover {
+            background: #999;
+        }
+        body {
+            background-color: paleturquoise;
+        }
+         ::-webkit-scrollbar {
+            display: none;
+        }
+        .modal-dialog {
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        .modal-content {
+            height: auto;
+            min-height: 100%;
+            border-radius: 0;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top2">
+        <a class="navbar-brand" href="#" style="padding:10px 5px"><img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/285/3619558285_d381ab3d-7056-42b0-b28c-45e5ada65b45.png?cb=1598620132"></img>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form class="form-inline my-2 my-lg-0">
+                <a type="button" href="https://cyberkavach.epizy.com/login/login.html" class="btn btn-info" style="padding:11px 25px">Login</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a type="button" href="https://cyberkavach.epizy.com/login/signup.html" class="btn btn-info" style="padding:11px 25px">Sign Up</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </form>
+        </div>
+    </nav>
+    <br><br><br><br>
+    <div class="jumbotron" id="ishaan">
+        <h1 class="display-4">Welcome to Cyber Kavach</h1>
+        <p class="lead">The all in one destination where you will get to know everything about Cyber Safety.</p>
+        <hr class="my-4">
+        <p>From Fake News to Fun games ,Cyber Kavach empowers you with various Cyber Safety techniques to become more digitally empowered.</p>
+        <p>"Cyber Kavach doesn't believe in complexifying problems but to make solutions so simple that everyone can implement them"</p>
+        <a type="button" href="https://cyberkavach.epizy.com/login/signup.html" class="btn btn-outline-secondary" style="padding:11px 25px">Learn more</a>
+    </div>
+    <br><br><br><br>
+    <footer class="page-footer font-small blue pt-4" style="background-color:mintcream;">
+        <div class="container-fluid text-center text-md-left">
+            <div class="row">
+                <div class="col-md-6 mt-md-0 mt-3">
+                    <h5 class="text-uppercase">Cyber Safety</h5>
+                    <p>Cyber Security is trying to be safe on the internet and is the act of maximizing a user's awareness of personal safety and security risks to private information and property associated with using the internet, and the self-protection
+                        from computer crime.</p>
+                </div>
+                <hr class="clearfix w-100 d-md-none pb-3">
+                <div class="col-md-3 mb-md-0 mb-3">
+                    <h5 class="text-uppercase">Features</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a class="an" href="https://cyberkavach.epizy.com/login/login.html" style="color:blue">Case Studies</a>
+                        </li>
+                        <li>
+                            <a class="an" href="https://cyberkavach.epizy.com/login/login.html" style="color:blue">Latest-News</a>
+                        </li>
+                        <li>
+                            <a class="an" href="https://cyberkavach.epizy.com/login/login.html" style="color:blue">Fakebusters</a>
+                        </li>
+                        <li>
+                            <a class="an" href="https://cyberkavach.epizy.com/login/login.html" style="color:blue">Cyber Honors</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 mb-md-0 mb-3">
+                    <h5 class="text-uppercase">Fun Zone</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a class="an" href="https://cyberkavach.epizy.com/login/login.html" style="color:blue"> Digital India Quiz</a>
+                        </li>
+                        <li>
+                            <a class="an" href="https://cyberkavach.epizy.com/login/login.html" style="color:blue">Cyberthon</a>
+                        </li>
+                        <li>
+                            <a class="an" href="https://cyberkavach.epizy.com/login/login.html" style="color:blue">Securify</a>
+                        </li>
+                        <li>
+                            <a class="an" href="https://cyberkavach.epizy.com/login/login.html" style="color:blue">Privacy Junction</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright text-center py-3" style="background-color: rgb(36, 36, 36); color: white;">2020<br>
+            <a>Cyber Kavach</a>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
